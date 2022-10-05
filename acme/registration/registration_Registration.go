bc4a483aa6e6c35ef20e719411c53e878d04b632
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-10-05 00:26:43 +0000

    chore(release): v3.0.2

--- acme/registration/registration_Registration.go
+++ acme/registration/registration_Registration.go
@@ -2,10 +2,10 @@ package registration
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-acme-go/acme/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-acme-go/acme/v3/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/hashicorp/cdktf-provider-acme-go/acme/v3/registration/internal"
+	"github.com/cdktf/cdktf-provider-acme-go/acme/v3/registration/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
