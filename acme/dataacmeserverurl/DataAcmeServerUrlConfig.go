edfbb7be66049350eda5667b81bbbd49270511ea
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2024-06-13 03:26:44 +0000

    chore(release): v11.4.0

--- acme/dataacmeserverurl/DataAcmeServerUrlConfig.go
+++ acme/dataacmeserverurl/DataAcmeServerUrlConfig.go
@@ -1,13 +1,13 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-package registration
+package dataacmeserverurl
 
 import (
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
-type RegistrationConfig struct {
+type DataAcmeServerUrlConfig struct {
 	// Experimental.
 	Connection interface{} `field:"optional" json:"connection" yaml:"connection"`
 	// Experimental.
@@ -22,15 +22,7 @@ type RegistrationConfig struct {
 	Provider cdktf.TerraformProvider `field:"optional" json:"provider" yaml:"provider"`
 	// Experimental.
 	Provisioners *[]interface{} `field:"optional" json:"provisioners" yaml:"provisioners"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/vancluever/acme/2.23.1/docs/resources/registration#account_key_pem Registration#account_key_pem}.
-	AccountKeyPem *string `field:"required" json:"accountKeyPem" yaml:"accountKeyPem"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/vancluever/acme/2.23.1/docs/resources/registration#email_address Registration#email_address}.
-	EmailAddress *string `field:"required" json:"emailAddress" yaml:"emailAddress"`
-	// external_account_binding block.
-	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/vancluever/acme/2.23.1/docs/resources/registration#external_account_binding Registration#external_account_binding}
-	ExternalAccountBinding *RegistrationExternalAccountBinding `field:"optional" json:"externalAccountBinding" yaml:"externalAccountBinding"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/vancluever/acme/2.23.1/docs/resources/registration#id Registration#id}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/vancluever/acme/2.23.1/docs/data-sources/server_url#id DataAcmeServerUrl#id}.
 	//
 	// Please be aware that the id field is automatically added to all resources in Terraform providers using a Terraform provider SDK version below 2.
 	// If you experience problems setting this value it might not be settable. Please take a look at the provider documentation to ensure it should be settable.
---
+++ acme/dataacmeserverurl/DataAcmeServerUrl__no_checks.go
@@ -0,0 +1,89 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+//go:build no_runtime_type_checking
+
+package dataacmeserverurl
+
+// Building without runtime type checking enabled, so all the below just return nil
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateAddOverrideParameters(path *string, value interface{}) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateGetAnyMapAttributeParameters(terraformAttribute *string) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateGetBooleanAttributeParameters(terraformAttribute *string) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateGetBooleanMapAttributeParameters(terraformAttribute *string) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateGetListAttributeParameters(terraformAttribute *string) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateGetNumberAttributeParameters(terraformAttribute *string) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateGetNumberListAttributeParameters(terraformAttribute *string) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateGetNumberMapAttributeParameters(terraformAttribute *string) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateGetStringAttributeParameters(terraformAttribute *string) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateGetStringMapAttributeParameters(terraformAttribute *string) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateInterpolationForAttributeParameters(terraformAttribute *string) error {
+	return nil
+}
+
+func (d *jsiiProxy_DataAcmeServerUrl) validateOverrideLogicalIdParameters(newLogicalId *string) error {
+	return nil
+}
+
+func validateDataAcmeServerUrl_GenerateConfigForImportParameters(scope constructs.Construct, importToId *string, importFromId *string) error {
+	return nil
+}
+
+func validateDataAcmeServerUrl_IsConstructParameters(x interface{}) error {
+	return nil
+}
+
+func validateDataAcmeServerUrl_IsTerraformDataSourceParameters(x interface{}) error {
+	return nil
+}
+
+func validateDataAcmeServerUrl_IsTerraformElementParameters(x interface{}) error {
+	return nil
+}
+
+func (j *jsiiProxy_DataAcmeServerUrl) validateSetCountParameters(val interface{}) error {
+	return nil
+}
+
+func (j *jsiiProxy_DataAcmeServerUrl) validateSetIdParameters(val *string) error {
+	return nil
+}
+
+func (j *jsiiProxy_DataAcmeServerUrl) validateSetLifecycleParameters(val *cdktf.TerraformResourceLifecycle) error {
+	return nil
+}
+
+func validateNewDataAcmeServerUrlParameters(scope constructs.Construct, id *string, config *DataAcmeServerUrlConfig) error {
+	return nil
+}
+
